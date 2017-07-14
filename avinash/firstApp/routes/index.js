b6f8b743ae8d9c3de51bd53ad528fb3163e6b670
jitendraavinash <jitendraavinash96@gmail.com>
2017-07-14 11:00:00 +0530

    some new code is added in index.js

--- avinash/firstApp/routes/index.js
+++ avinash/firstApp/routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page. 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', name: 'Avinash' });
+    res.render('index', { title: 'Express', name: 'Avinash' });
+  });
+*/
+
+/*router.get('/:id', function(req, res){
+   res.send('The id you specified is ' + req.params.id);
+});*/
+
+
+router.use(function(req, res, next){
+   console.log("A new request received at " + Date.now());
+   next();
 });
+
+router.use('/', function(req, res){
+   console.log('End');
+});
+
+
+router.get('/:id([0-9]{5})', function(req, res){
+   res.send('id: ' + req.params.id);
+   next();
+});
+
+
 
 module.exports = router;
